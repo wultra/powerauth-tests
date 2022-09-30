7d44831a3b286080fb21111f8a112d219e9e676e
Lubos Racansky <lubos.racansky@gmail.com>
2022-09-30 14:36:44 +0200

    Fix #111: Add test for mobile app version endpoint

---
+++ powerauth-backend-tests/src/test/java/com/wultra/security/powerauth/test/v31/PowerAuthEnrollmentConfigurationTest.java
@@ -0,0 +1,81 @@
+/*
+ * PowerAuth test and related software components
+ * Copyright (C) 2022 Wultra s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.wultra.security.powerauth.test.v31;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.wultra.security.powerauth.configuration.PowerAuthTestConfiguration;
+import io.getlime.security.powerauth.lib.cmd.logging.ObjectStepLogger;
+import io.getlime.security.powerauth.lib.cmd.steps.model.EncryptStepModel;
+import io.getlime.security.powerauth.lib.cmd.steps.v3.EncryptStep;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Test for enrollment configuration endpoint.
+ *
+ * @author Lubos Racansky, lubos.racansky@wultra.com
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = PowerAuthTestConfiguration.class)
+@EnableConfigurationProperties
+class PowerAuthEnrollmentConfigurationTest {
+
+    @Autowired
+    private PowerAuthTestConfiguration config;
+
+    private EncryptStepModel encryptModel;
+
+    private final ObjectMapper objectMapper = new ObjectMapper().disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
+    @BeforeEach
+    void setUp() throws Exception {
+        encryptModel = new EncryptStepModel();
+        encryptModel.setApplicationKey(config.getApplicationKey());
+        encryptModel.setApplicationSecret(config.getApplicationSecret());
+        encryptModel.setMasterPublicKey(config.getMasterPublicKey());
+        encryptModel.setHeaders(new HashMap<>());
+        encryptModel.setResultStatusObject(config.getResultStatusObjectV31());
+        encryptModel.setVersion("3.1");
+    }
+
+    @Test
+    void testConfiguration() throws Exception {
+        encryptModel.setData(objectMapper.writeValueAsBytes("{}"));
+        encryptModel.setUriString(config.getEnrollmentServiceUrl() + "/api/configuration");
+        encryptModel.setScope("activation");
+
+        final ObjectStepLogger stepLogger = new ObjectStepLogger(System.out);
+        new EncryptStep().execute(stepLogger, encryptModel.toMap());
+        assertTrue(stepLogger.getResult().isSuccess());
+        assertEquals(200, stepLogger.getResponse().getStatusCode());
+
+        final Object response = stepLogger.getResponse().getResponseObject();
+        // TODO Lubos
+    }
+}
