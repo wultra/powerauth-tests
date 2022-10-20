e0812ac2af82b4def611c2059d562d7b5f12117a
Lubos Racansky <lubos.racansky@gmail.com>
2022-10-20 10:08:59 +0200

    Fix #80: Implement tests for onboarding process limits
    - testFailedActivationOtpMaxFailedAttemptsFailProcess

--- powerauth-backend-tests/src/test/java/com/wultra/security/powerauth/test/v31/PowerAuthIdentityVerificationTest.java
+++ powerauth-backend-tests/src/test/java/com/wultra/security/powerauth/test/v31/PowerAuthIdentityVerificationTest.java
@@ -698,14 +698,30 @@ class PowerAuthIdentityVerificationTest {
         powerAuthClient.removeActivation(activationId, "test");
     }
 
+    @Test
+    void testFailedActivationOtpMaxFailedAttemptsFailProcess() throws Exception {
+        final String clientId = generateRandomClientId();
+        final String processId = startOnboarding(clientId);
+
+        for (int i = 0; i < 4; i++) {
+            createCustomActivationWrongOtp(processId, clientId);
+            final OnboardingStatus status = checkProcessStatus(processId);
+            assertEquals(OnboardingStatus.ACTIVATION_IN_PROGRESS, status);
+        }
+
+        createCustomActivationWrongOtp(processId, clientId);
+        final OnboardingStatus status = checkProcessStatus(processId);
+        assertEquals(OnboardingStatus.FAILED, status);
+    }
+
     private TestContext prepareActivation() throws Exception {
         return prepareActivation("");
     }
 
     private TestContext prepareActivation(final String clientIdPostfix) throws Exception {
-        String clientId = generateRandomClientId() + clientIdPostfix;
-        String processId = startOnboarding(clientId);
-        String activationId = createCustomActivation(processId, getOtpCode(processId, OtpType.ACTIVATION), clientId);
+        final String clientId = generateRandomClientId() + clientIdPostfix;
+        final String processId = startOnboarding(clientId);
+        final String activationId = createCustomActivation(processId, getOtpCode(processId, OtpType.ACTIVATION), clientId);
         createToken();
 
         final TestContext testContext = new TestContext();
@@ -767,14 +783,27 @@ class PowerAuthIdentityVerificationTest {
     }
 
     private String createCustomActivation(String processId, String otpCode, String clientId) throws Exception {
+        return createCustomActivation(processId, otpCode, clientId, true);
+    }
+
+    private void createCustomActivationWrongOtp(String processId, String clientId) throws Exception {
+        createCustomActivation(processId, "wrong otp", clientId, false);
+    }
+
+    private String createCustomActivation(String processId, String otpCode, String clientId, boolean expectedResult) throws Exception {
         stepLogger = new ObjectStepLogger(System.out);
-        Map<String, String> identityAttributes = new HashMap<>();
+        final Map<String, String> identityAttributes = new HashMap<>();
         identityAttributes.put("processId", processId);
         identityAttributes.put("otpCode", otpCode);
         identityAttributes.put("credentialsType", "ONBOARDING");
         activationModel.setIdentityAttributes(identityAttributes);
         new CreateActivationStep().execute(stepLogger, activationModel.toMap());
-        assertTrue(stepLogger.getResult().isSuccess());
+        assertEquals(expectedResult, stepLogger.getResult().isSuccess());
+
+        if (!expectedResult) {
+            return null;
+        }
+
         assertEquals(200, stepLogger.getResponse().getStatusCode());
 
         String activationId = null;
