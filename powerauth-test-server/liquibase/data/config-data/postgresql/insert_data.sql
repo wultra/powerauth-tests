febf24b7a18abf56caec1a7f3b920afec17f9ebb
jan.herza <jan.herza@wultra.com>
2023-02-08 01:25:31 +0100

    liquibase parameters updated

--- powerauth-test-server/liquibase/data/config-data/postgresql/insert_data.sql
+++ powerauth-test-server/liquibase/data/config-data/postgresql/insert_data.sql
@@ -54,11 +54,11 @@ INSERT INTO pa_cloud_localization (id, placeholder, language, title, summary)
 
 -- admin user
 INSERT INTO pa_cloud_user (id, username, password, enabled)
-    VALUES (nextval('pa_cloud_user_seq'), '${POWERAUTH_CLOUD_ADMIN_USERNAME}',
-        '${POWERAUTH_CLOUD_ADMIN_PASSWORD_ENCODED}', true)
+    VALUES (nextval('pa_cloud_user_seq'), '${PA_CLOUD_ADMIN_USERNAME}',
+        '${PA_CLOUD_ADMIN_PASSWORD_ENCODED}', true)
     ON CONFLICT (username) DO NOTHING;
 
 INSERT INTO pa_cloud_user_authority (id, user_id, authority)
     VALUES (nextval('pa_cloud_user_seq'),
-        (SELECT id FROM pa_cloud_user WHERE username = '${POWERAUTH_CLOUD_ADMIN_USERNAME}'), 'ROLE_ADMIN')
+        (SELECT id FROM pa_cloud_user WHERE username = '${PA_CLOUD_ADMIN_USERNAME}'), 'ROLE_ADMIN')
     ON CONFLICT (user_id, authority) do nothing;
