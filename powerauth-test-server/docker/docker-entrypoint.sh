90bac6672d13594ea5f34bef7967ce43a75221c6
jan.herza <jan.herza@wultra.com>
2023-02-08 13:26:18 +0100

    docker-entrypoint.sh deleted - will not be used

--- powerauth-test-server/docker/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-liquibase --headless=true --log-level=INFO --changeLogFile=$LB_HOME/data/changelog.xml --username=$POWERAUTH_CLOUD_DATASOURCE_USERNAME --password=$POWERAUTH_CLOUD_DATASOURCE_PASSWORD --url=$POWERAUTH_CLOUD_DATASOURCE_URL update
-
-catalina.sh run
