450729bbc64c9afde9897e112b5d0bde18e3134c
Jan Dusil <134381434+jandusil@users.noreply.github.com>
2024-01-26 10:58:11 +0100

    Fix #278: Add request validation for Test Server endpoints (#342)
    * Fix ##278: Add request validation for Test Server endpoints

--- powerauth-test-server/src/main/java/com/wultra/security/powerauth/app/testserver/model/request/OperationApproveInternalRequest.java
+++ powerauth-test-server/src/main/java/com/wultra/security/powerauth/app/testserver/model/request/OperationApproveInternalRequest.java
@@ -17,6 +17,7 @@
  */
 package com.wultra.security.powerauth.app.testserver.model.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 /**
@@ -26,11 +27,14 @@ import lombok.Data;
  */
 @Data
 public class OperationApproveInternalRequest {
-
+    @NotBlank
     private String activationId;
+    @NotBlank
     private String applicationId;
     private String password;
+    @NotBlank
     private String operationId;
+    @NotBlank
     private String operationData;
 
 }
