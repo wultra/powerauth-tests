8468110877c5172cc846e51ffd8a0bc045b94343
Jan Pešek <jan.pesek@wultra.com>
2024-03-08 09:01:15 +0100

    Fix #386: FIDO2 Test application: Frontend

--- powerauth-fido2-tests/src/main/java/com/wultra/security/powerauth/fido2/controller/HomeController.java
+++ powerauth-fido2-tests/src/main/java/com/wultra/security/powerauth/fido2/controller/HomeController.java
@@ -18,8 +18,16 @@
 
 package com.wultra.security.powerauth.fido2.controller;
 
+import com.wultra.security.powerauth.client.model.error.PowerAuthClientException;
+import com.wultra.security.powerauth.fido2.service.Fido2SharedService;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.Map;
 
@@ -29,11 +37,58 @@ import java.util.Map;
  * @author Jan Pesek, jan.pesek@wultra.com
  */
 @Controller
+@AllArgsConstructor
+@Slf4j
 public class HomeController {
 
-    @GetMapping
+    private static final String REDIRECT_LOGIN = "redirect:/login";
+    private static final String LOGIN_PAGE = "/login";
+    private static final String PAYMENT_PAGE = "/payment";
+    private static final String ERROR_PAGE = "/error";
+
+    private final Fido2SharedService sharedService;
+    private final ServletContext context;
+
+    @ModelAttribute
+    public void addAttributes(Map<String, Object> model) {
+        model.put("servletContextPath", context.getContextPath());
+    }
+
+    @GetMapping("/")
     public String homePage(Map<String, Object> model) {
-        return "home";
+        return REDIRECT_LOGIN;
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Map<String, Object> model) {
+        try {
+            model.put("applications", sharedService.fetchApplicationNameList());
+            model.put("templates", sharedService.fetchTemplateNameList());
+            return LOGIN_PAGE;
+        } catch (PowerAuthClientException ex) {
+            logger.warn(ex.getMessage(), ex);
+            model.put("exception", ex);
+            model.put("errorMessage", ex.getMessage());
+            return ERROR_PAGE;
+        }
+    }
+
+    @GetMapping("/payment")
+    public String profilePage(Map<String, Object> model) {
+        try {
+            model.put("templates", sharedService.fetchTemplateNameList());
+            return PAYMENT_PAGE;
+        } catch (PowerAuthClientException ex) {
+            logger.warn(ex.getMessage(), ex);
+            model.put("exception", ex);
+            model.put("errorMessage", ex.getMessage());
+            return ERROR_PAGE;
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage(Map<String, Object> model) {
+        return REDIRECT_LOGIN;
     }
 
 }
