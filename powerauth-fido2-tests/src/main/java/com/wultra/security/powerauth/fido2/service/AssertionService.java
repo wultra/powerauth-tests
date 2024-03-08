425f979cb78142f971bca08d9ed734afb76d6a67
Jan Pešek <jan.pesek@wultra.com>
2024-03-08 08:50:24 +0100

    Fix #379: FIDO2 Test application: Backend (#385)

---
+++ powerauth-fido2-tests/src/main/java/com/wultra/security/powerauth/fido2/service/AssertionService.java
@@ -0,0 +1,124 @@
+/*
+ * PowerAuth test and related software components
+ * Copyright (C) 2024 Wultra s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.wultra.security.powerauth.fido2.service;
+
+import com.wultra.security.powerauth.client.PowerAuthFido2Client;
+import com.wultra.security.powerauth.client.model.error.PowerAuthClientException;
+import com.wultra.security.powerauth.client.model.request.fido2.AssertionChallengeRequest;
+import com.wultra.security.powerauth.client.model.request.fido2.AssertionVerificationRequest;
+import com.wultra.security.powerauth.client.model.response.fido2.AssertionChallengeResponse;
+import com.wultra.security.powerauth.client.model.response.fido2.AssertionVerificationResponse;
+import com.wultra.security.powerauth.fido2.configuration.WebAuthnConfiguration;
+import com.wultra.security.powerauth.fido2.controller.request.AssertionOptionsRequest;
+import com.wultra.security.powerauth.fido2.controller.request.VerifyAssertionRequest;
+import com.wultra.security.powerauth.fido2.controller.response.AssertionOptionsResponse;
+import com.wultra.security.powerauth.fido2.controller.response.CredentialDescriptor;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Service for WebAuthn authentication tasks.
+ *
+ * @author Jan Pesek, jan.pesek@wultra.com
+ */
+@Service
+@AllArgsConstructor
+@Slf4j
+public class AssertionService {
+
+    private static final String OPERATION_DATA_EXTENSION_KEY = "txAuthSimple";
+
+    private final PowerAuthFido2Client fido2Client;
+    private final Fido2SharedService fido2SharedService;
+    private final WebAuthnConfiguration webAuthNConfig;
+
+    /**
+     * Build public key assertion options.
+     * @param request Request form with user input.
+     * @return Public key assertion options.
+     * @throws PowerAuthClientException in case of PowerAuth server communication error.
+     */
+    public AssertionOptionsResponse assertionOptions(final AssertionOptionsRequest request) throws PowerAuthClientException {
+        final String userId = request.username();
+        final String applicationId = request.applicationId();
+
+        logger.info("Building assertion options for userId={}, applicationId={}", userId, applicationId);
+
+        final List<CredentialDescriptor> existingCredentials = fido2SharedService.fetchExistingCredentials(userId, applicationId);
+        if (existingCredentials.isEmpty() && StringUtils.hasText(userId))  {
+            throw new IllegalStateException("Not registered yet.");
+        }
+
+        final AssertionChallengeResponse challengeResponse = fetchChallenge(applicationId, request.templateName(), request.operationParameters());
+        final String challenge = challengeResponse.getChallenge();
+        final String operationData = extractOperationData(challenge);
+
+        return AssertionOptionsResponse.builder()
+                .challenge(challenge)
+                .rpId(webAuthNConfig.getRpId())
+                .timeout(webAuthNConfig.getTimeout().toMillis())
+                .allowCredentials(existingCredentials)
+                .extensions(Map.of(OPERATION_DATA_EXTENSION_KEY, operationData))
+                .build();
+    }
+
+    /**
+     * Verify credential at PowerAuth server.
+     * @param credential Received public key credential.
+     * @return PowerAuth authentication response.
+     * @throws PowerAuthClientException in case of PowerAuth server communication error.
+     */
+    public AssertionVerificationResponse authenticate(final VerifyAssertionRequest credential) throws PowerAuthClientException {
+        final AssertionVerificationRequest request = new AssertionVerificationRequest();
+        request.setId(credential.id());
+        request.setType(credential.type().getValue());
+        request.setAuthenticatorAttachment(credential.authenticatorAttachment().getValue());
+        request.setResponse(credential.response());
+        request.setApplicationId(credential.applicationId());
+        request.setExpectedChallenge(credential.expectedChallenge());
+        request.setRelyingPartyId(webAuthNConfig.getRpId());
+        request.setAllowedOrigins(webAuthNConfig.getAllowedOrigins());
+        request.setRequiresUserVerification(credential.userVerificationRequired());
+        return fido2Client.authenticate(request);
+    }
+
+    private AssertionChallengeResponse fetchChallenge(final String applicationId, final String templateName, final Map<String, String> operationParameters) throws PowerAuthClientException {
+        final AssertionChallengeRequest request = new AssertionChallengeRequest();
+        request.setApplicationIds(List.of(applicationId));
+        request.setTemplateName(templateName);
+        if (operationParameters != null) {
+            request.setParameters(operationParameters);
+        }
+        return fido2Client.requestAssertionChallenge(request);
+    }
+
+    private static String extractOperationData(final String challenge) {
+        final String[] split = challenge.split("&", 2);
+        if (split.length != 2) {
+            throw new IllegalStateException("Invalid challenge.");
+        }
+        return split[1];
+    }
+
+}
---
+++ powerauth-fido2-tests/src/main/java/com/wultra/security/powerauth/fido2/service/Fido2SharedService.java
@@ -0,0 +1,106 @@
+/*
+ * PowerAuth test and related software components
+ * Copyright (C) 2024 Wultra s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.wultra.security.powerauth.fido2.service;
+
+import com.webauthn4j.data.AuthenticatorTransport;
+import com.webauthn4j.data.PublicKeyCredentialType;
+import com.wultra.security.powerauth.client.PowerAuthClient;
+import com.wultra.security.powerauth.client.PowerAuthFido2Client;
+import com.wultra.security.powerauth.client.model.entity.Application;
+import com.wultra.security.powerauth.client.model.entity.fido2.AuthenticatorDetail;
+import com.wultra.security.powerauth.client.model.error.PowerAuthClientException;
+import com.wultra.security.powerauth.client.model.response.OperationTemplateDetailResponse;
+import com.wultra.security.powerauth.fido2.controller.response.CredentialDescriptor;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Service shared for registration and authentication.
+ *
+ * @author Jan Pesek, jan.pesek@wultra.com
+ */
+@Service
+@AllArgsConstructor
+@Slf4j
+public class Fido2SharedService {
+
+    private static final String EXTRAS_TRANSPORT_KEY = "transports";
+    private static final PublicKeyCredentialType CREDENTIAL_TYPE = PublicKeyCredentialType.PUBLIC_KEY;
+
+    private final PowerAuthFido2Client fido2Client;
+    private final PowerAuthClient powerAuthClient;
+
+    /**
+     * Fetch all registered credentials.
+     * @param userId User to whom the credentials belong.
+     * @param applicationId Of the used application.
+     * @return List of credentials.
+     * @throws PowerAuthClientException if there is an error in PowerAuth communication.
+     */
+    public List<CredentialDescriptor> fetchExistingCredentials(final String userId, final String applicationId) throws PowerAuthClientException {
+        if (!StringUtils.hasText(userId) || !StringUtils.hasText(applicationId)) {
+            return Collections.emptyList();
+        }
+
+        return listAuthenticators(userId, applicationId).stream()
+                .map(Fido2SharedService::toCredentialDescriptor)
+                .toList();
+    }
+
+    /**
+     * Fetch list of all existing applications.
+     * @return List of application ids.
+     * @throws PowerAuthClientException if there is an error in PowerAuth communication.
+     */
+    public List<String> fetchApplicationNameList() throws PowerAuthClientException {
+        return powerAuthClient.getApplicationList().getApplications()
+                .stream()
+                .map(Application::getApplicationId)
+                .sorted().toList();
+    }
+
+    /**
+     * Fetch all existing operation templates.
+     * @return List of operation template names.
+     * @throws PowerAuthClientException if there is an error in PowerAuth communication.
+     */
+    public List<String> fetchTemplateNameList() throws PowerAuthClientException {
+        return powerAuthClient.operationTemplateList()
+                .stream()
+                .map(OperationTemplateDetailResponse::getTemplateName)
+                .sorted().toList();
+    }
+
+    private List<AuthenticatorDetail> listAuthenticators(final String userId, final String applicationId) throws PowerAuthClientException {
+        return fido2Client.getRegisteredAuthenticatorList(userId, applicationId).getAuthenticators();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static CredentialDescriptor toCredentialDescriptor(final AuthenticatorDetail authenticatorDetail) {
+        final String credentialId = authenticatorDetail.getExternalId();
+        final List<AuthenticatorTransport> transports = (List<AuthenticatorTransport>) authenticatorDetail.getExtras().getOrDefault(EXTRAS_TRANSPORT_KEY, Collections.emptyList());
+        return new CredentialDescriptor(CREDENTIAL_TYPE, credentialId, transports);
+    }
+
+}
