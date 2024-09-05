18afc1cf3ab5db98546fc36d441d569b3a990994
Jan Pešek <jan.pesek@wultra.com>
2024-09-05 14:19:01 +0100

    Use AbstractEmailValidator

--- powerauth-fido2-tests/src/main/java/com/wultra/security/powerauth/fido2/controller/validation/EmailConditionalValidator.java
+++ powerauth-fido2-tests/src/main/java/com/wultra/security/powerauth/fido2/controller/validation/EmailConditionalValidator.java
@@ -19,9 +19,9 @@
 package com.wultra.security.powerauth.fido2.controller.validation;
 
 import com.wultra.security.powerauth.fido2.configuration.PowerAuthFido2TestsConfigProperties;
-import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.AllArgsConstructor;
+import org.hibernate.validator.internal.constraintvalidators.AbstractEmailValidator;
 import org.springframework.util.StringUtils;
 
 import java.util.regex.Pattern;
@@ -32,21 +32,19 @@ import java.util.regex.Pattern;
  * @author Jan Pesek, jan.pesek@wultra.com
  */
 @AllArgsConstructor
-public class EmailConditionalValidator implements ConstraintValidator<EmailConditional, String> {
+public class EmailConditionalValidator extends AbstractEmailValidator<EmailConditional> {
 
-    private static final Pattern EMAIL_PATTERN = Pattern.compile(
-            "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
-    );
+    private static final Pattern GENERIC_EMAIL_PATTERN = Pattern.compile("[^@\\s]+@[^@\\s]+\\.[^@\\s]+");
 
     private final PowerAuthFido2TestsConfigProperties powerAuthFido2TestsConfigProperties;
 
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
+    public boolean isValid(final CharSequence value, final ConstraintValidatorContext context) {
         if (!StringUtils.hasLength(value) || !powerAuthFido2TestsConfigProperties.isEmailAddressRequired()) {
             return true;
         }
 
-        return EMAIL_PATTERN.matcher(value).matches();
+        return super.isValid(value, context) && GENERIC_EMAIL_PATTERN.matcher(value).matches();
     }
 
 }
