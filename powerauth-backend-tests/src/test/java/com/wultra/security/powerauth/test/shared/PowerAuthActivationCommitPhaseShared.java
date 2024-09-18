9e36f5b1154057d119693c5d17f9a2d7139fc30d
Roman Strobl <roman.strobl@wultra.com>
2024-09-18 15:49:20 +0800

    Update copyrights

--- powerauth-backend-tests/src/test/java/com/wultra/security/powerauth/test/shared/PowerAuthActivationCommitPhaseShared.java
+++ powerauth-backend-tests/src/test/java/com/wultra/security/powerauth/test/shared/PowerAuthActivationCommitPhaseShared.java
@@ -1,6 +1,6 @@
 /*
  * PowerAuth test and related software components
- * Copyright (C) 2023 Wultra s.r.o.
+ * Copyright (C) 2024 Wultra s.r.o.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
