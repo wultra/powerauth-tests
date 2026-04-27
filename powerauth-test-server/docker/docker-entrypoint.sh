f4956338b54937ebb963301bdb69754f3e96adb7
filipbursik-wultra <filip.bursik@wultra.com>
2026-04-27 17:45:26 +0200

    Refactor Docker setup and update Liquibase changelog paths (#874)
    (cherry picked from commit 625f43bf100a57aff895903c2a3253bafdd9d1fd)

--- powerauth-test-server/docker/docker-entrypoint.sh
+++ powerauth-test-server/docker/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 if [ "${LQ_ENABLED}" = true ]; then
-  liquibase --headless=true --log-level=INFO --changeLogFile="${LB_HOME}/data/powerauth-test-server/db.changelog-module.xml" --username="${POWERAUTH_TEST_SERVER_DATASOURCE_USERNAME}" --password="${POWERAUTH_TEST_SERVER_DATASOURCE_PASSWORD}" --url="${POWERAUTH_TEST_SERVER_DATASOURCE_URL}" update
+  liquibase --headless=true --log-level=INFO --changeLogFile="${LB_HOME}/db/changelog/db.changelog-module.xml" --username="${POWERAUTH_TEST_SERVER_DATASOURCE_USERNAME}" --password="${POWERAUTH_TEST_SERVER_DATASOURCE_PASSWORD}" --url="${POWERAUTH_TEST_SERVER_DATASOURCE_URL}" update
 fi
 
-exec java -Dserver.port=8080 -Dspring.config.additional-location=/app/application.properties ${JAVA_OPTS:-} -Dserver.servlet.context-path=/powerauth-test-server -cp "${APP_PATH}:/app/extlib/*" org.springframework.boot.loader.launch.WarLauncher
+exec java -Dserver.port=8080 ${JAVA_OPTS:-} -Dserver.servlet.context-path=/powerauth-test-server -cp "${APP_PATH}:/app/extlib/*" org.springframework.boot.loader.launch.WarLauncher
