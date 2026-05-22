379340a2101546f05e5c308fb4cf01371df5b878
Luboš Račanský <lubos.racansky@wultra.com>
2026-05-22 10:53:45 +0200

    Fix #888: Set consentRequired for reactivation tests (#889)
    * Fix #888: Set consentRequired for reactivation tests
    * add approveConsent to initDocumentVerificationSdkTest

--- powerauth-backend-tests/src/test/resources/data/enrollment-onboarding/extra-data.sql
+++ powerauth-backend-tests/src/test/resources/data/enrollment-onboarding/extra-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO es_onboarding_process_configuration (id, process_type, config) VALUES
-    -- activationType IDENTITY, with OTPs
-    (1, 'reactivation', '{"enabled":true,"activationType":"IDENTITY","otpForIdentification":true,"otpForIdentityVerification":true,"otpResendPeriodSeconds":30,"documents":{"totalRequiredDocumentsCount":2,"groups":[{"requiredDocumentsCount":1,"items":[{"type":"ID_CARD","sideCount":2}]},{"requiredDocumentsCount":1,"items":[{"type":"PASSPORT","sideCount":1},{"type":"DRIVING_LICENSE","sideCount":1}]}]}}'),
+    -- activationType IDENTITY, with OTPs, consentRequired
+    (1, 'reactivation', '{"enabled":true,"activationType":"IDENTITY","otpForIdentification":true,"otpForIdentityVerification":true,"consentRequired":true,"otpResendPeriodSeconds":30,"documents":{"totalRequiredDocumentsCount":2,"groups":[{"requiredDocumentsCount":1,"items":[{"type":"ID_CARD","sideCount":2}]},{"requiredDocumentsCount":1,"items":[{"type":"PASSPORT","sideCount":1},{"type":"DRIVING_LICENSE","sideCount":1}]}]}}'),
 
     -- activationType CODE, without OTPs, with temp activation, with approval
     (2, 'onboarding', '{"enabled":true,"activationType":"CODE","otpForIdentification":false,"otpForIdentityVerification":false,"otpResendPeriodSeconds":30,"useTemporaryActivation":true,"approvalEnabled":true,"verifyPresenceWithOtp":false,"documents":{"totalRequiredDocumentsCount":2,"groups":[{"requiredDocumentsCount":1,"items":[{"type":"ID_CARD","sideCount":2}]},{"requiredDocumentsCount":1,"items":[{"type":"PASSPORT","sideCount":1},{"type":"DRIVING_LICENSE","sideCount":1}]}]}}'),
